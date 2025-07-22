ce339f3fa9d794c1e01d24432f666cf89897fefb
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-22 21:00:44 +0500

    up

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -4,7 +4,9 @@ config()
 export const configServer = {
     PORT: process.env.PORT,
     mongodb_url: process.env.MONGODB_URL,
-    SUPERADMIN_USERNAME: process.env.SUPERADMIN_USERNAME,
-    SUPERADMIN_EMAIL: process.env.SUPERADMIN_EMAIL,
-    SUPERADMIN_PASSWORD: process.env.SUPERADMIN_PASSWORD
+    ADMIN: {
+        SUPERADMIN_USERNAME: process.env.SUPERADMIN_USERNAME,
+        SUPERADMIN_EMAIL: process.env.SUPERADMIN_EMAIL,
+        SUPERADMIN_PASSWORD: process.env.SUPERADMIN_PASSWORD
+    }
 }
