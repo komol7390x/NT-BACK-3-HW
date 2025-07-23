204920b13f6714d99297e8bf2cc83a1ddd848b40
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-23 22:54:08 +0500

    up

--- src/routers/admin.route.js
+++ src/routers/admin.route.js
@@ -2,16 +2,18 @@ import controller from '../controller/admin.controller.js'
 import { Router } from 'express'
 import { RolesGuard } from '../guards/role.guard.js'
 import AuthGuard from '../guards/auth.guard.js'
+import Adminvalidate from '../validation/admin.validat.js'
+import { validate } from '../middlewares/validate.js'
 const router = Router()
 
 router
-    .post('/', AuthGuard, RolesGuard('SUPERADMIN'), controller.createAdmin)
-    .post('/signin', controller.signInAdmin)
+    .post('/', AuthGuard, RolesGuard('SUPERADMIN'), validate(Adminvalidate.create), controller.createAdmin)
+    .post('/signin', validate(Adminvalidate.signIn), controller.signInAdmin)
     .post('/newtoken', controller.newToken)
     .post('/logout', AuthGuard, controller.signOut)
     .get('/', AuthGuard, RolesGuard('SUPERADMIN'), controller.getAll)
     .get('/:id', AuthGuard, RolesGuard('SUPERADMIN', 'ID'), controller.getByID)
-    .patch('/:id', AuthGuard, RolesGuard('SUPERADMIN', 'ID'), controller.update)
+    .patch('/:id', AuthGuard, RolesGuard('SUPERADMIN', 'ID'), validate(Adminvalidate.update), controller.update)
     .delete('/:id', RolesGuard('SUPERADMIN'), AuthGuard, controller.delete)
 
 export default router
