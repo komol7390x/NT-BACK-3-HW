e174cd64a070e441daa259786fcbef418da08786
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-24 22:17:56 +0500

    up

--- src/error/page-not-found.js
+++ src/error/page-not-found.js
@@ -1,4 +1,4 @@
-import { AppError } from "./AppError";
+import { AppError } from "./AppError.js";
 
 export const pageError = (_req, _res, next) => {
     throw next(new AppError('Page not found', 404))
--- src/routers/index.route.js
+++ src/routers/index.route.js
@@ -1,9 +1,12 @@
 import express, { Router } from "express";
 import adminRouter from './admin.route.js'
 import clientRouter from './client.route.js'
+import { pageError } from "../error/page-not-found.js";
 const router = Router()
 
 router.use('/admin', adminRouter)
 router.use('/client', clientRouter)
+
+router.use(pageError)
 
 export default router
