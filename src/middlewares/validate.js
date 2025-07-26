c80a89e5bf79965efee357ce1720b323f374d0ce
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-26 12:40:35 +0500

    uo

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,11 +1,11 @@
-import {AppError} from '../error/'
+import { AppError } from '../error/AppError.js'
 export const validate = (schema) => {
     return (req, _, next) => {
         try {
             const validatSchema = schema();
             const { error } = validatSchema.validate(req.body);
             if (error) {
-                throw new AppError((error?.details[0]?.message ?? 'Error input validation'),422)
+                throw new AppError((error?.details[0]?.message ?? 'Error input validation'), 422)
             }
             next()
         } catch (error) {
