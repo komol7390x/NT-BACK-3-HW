3f062915b99f61f4666dcb9489452e74ee4bc845
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-07-30 20:32:51 +0500

    up

--- src/helper/log/logger.js
+++ src/helper/log/logger.js
@@ -5,6 +5,7 @@ import { configServer } from "../../config/server.config.js";
 const customTime = winston.format((info) => {
     const data = new Date();
     info.timestamp = data.toLocaleString('en-GB', { timeZone: 'Asia/Tashkent', hour12: false })
+    return info
 })
 
 const logger = winston.createLogger({
