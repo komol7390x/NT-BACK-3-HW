ffbb781670a713f6bebd371fe1f61cf811bd5e2c
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-08-01 11:24:21 +0500

    up

--- src/guards/role.guard.js
+++ src/guards/role.guard.js
@@ -4,7 +4,7 @@ export const RolesGuard = (...roles) => {
     return async (req, _res, next) => {
         try {            
             const role = await req.user.role
-            if (role && roles.includes(role)) {                                
+            if (role && roles.includes(role)) {                                                
                 return next()
             }
             throw new AppError('Forbidden user', 403)
