c17bb98c1e7c90bc710b592810bc334962f7796b
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-08-07 12:57:19 +0500

    up

--- src/database/psql.database.js
+++
@@ -1,9 +0,0 @@
-import { Pool } from "pg";
-import config from '../config/env.config.js'
-
-const pg = new Pool({
-    // postgres://<username>:<password>@<host>/<database>
-    connectionString: `${config.PSQL_URL}${config.PSQL_DATABASE}`
-})
-
-export default pg
