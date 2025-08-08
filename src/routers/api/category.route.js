fabfa5f712444bd39800d38e961bf01c4a7f3bed
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-08-08 19:17:10 +0500

    up

--- src/routers/api/category.route.js
+++ src/routers/api/category.route.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 
 import constroller from '../../controller/api/category.controller.js'
+import Validation from '../../validation/api/category.validate.js'
 
 import {AuthGuard} from '../../guards/auth.guard.js'
 import {RoleGuard} from '../../guards/role.guard.js'
 import { Role } from "../../const/Role.js";
 import { oneFile } from "../../middleware/upload.middle.js";
-import Validation from '../../validation/api/category.validate.js'
 import { validate } from "../../middleware/validate.middle.js";
 const router = Router()
 
